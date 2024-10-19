c388eed64e8d3db8b388fe80329a04b1e3a4e428
laajilisamar <ajilisamar.army@gmail.com>
2024-10-19 13:47:12 +0100

    First commit

---
+++ assets/data/doa.js
@@ -0,0 +1,45 @@
+const doa = [
+	{
+		nama : "دعاءالافطار",
+		arab : "اَللّهُمَّ لَكَ صُمْتُ وَبِكَ آمَنْتُ وَعَلَى رِزْقِكَ أَفْطَرْتُ بِرَحْمَتِكَ يَا اَرْحَمَ الرَّحِمِيْنَ",
+		latin : "Allahumma laka shumtu wa bika amantu wa'ala rizqika afthartu. Birrahmatika yaa arhamar roohimin.",
+		indo : "“O God, for You I have fasted, and in You I have believed, and with Your provision I have broken my fast, by Your mercy, O Most Merciful of the merciful.”",
+		warna : 1
+	},
+	{
+		nama : "دعاء الأكل",
+		arab : "اَللّٰهُمَّ بَارِكْ لَنَا فِيْمَا رَزَقْتَنَا وَقِنَا عَذَابَ النَّارِ",
+		latin : "Alloohumma barik lanaa fiimaa razatanaa waqinaa 'adzaa bannar",
+		indo : "Oh God, bless us with what You have provided us with and protect us from the torment of the Fire.",
+		warna : 4
+	},
+	{
+		nama : " دعاء عند ننسى بسملة عندالاكل",
+		arab : "اَلْحَمْدُ ِللهِ الَّذِىْ اَطْعَمَنَا وَسَقَانَا وَجَعَلَنَا مُسْلِمِيْنَ",
+		latin : "Alhamdu lillaahil ladzii ath'amanaa wa saqoonaa wa ja'alnaa muslimiin",
+		indo : "“Praise be to God who fed us, gave us drink, and made us Muslims.”",
+		warna : 3
+	},
+	{
+		nama : " دعاء عند الأكل " ,
+		arab : "بِسْمِ اللهِ فِىِ أَوَّلِهِ وَآخِرِهِ",
+		latin : "Bismillaahi fii awwalihi wa aakhirihi",
+		indo : "“In the name of God in its beginning and its end.”",
+		warna: 4
+	},
+	{
+		nama : " دعاء قبل النوم ",
+		arab : "بِسْمِكَ اللّٰهُمَّ اَحْيَا وَاَمُوْتُ",
+		latin : "Bismikallohumma ahya wa amuutu",
+		indo : "“In Your Name, O God, I live and die.”",
+		warna : 5
+	},
+	{
+		nama : "دعاء دخول الى الحمام",
+		arab : "اَللّٰهُمَّ اِنّىْ اَعُوْذُبِكَ مِنَ الْخُبُثِ وَالْخَبَآئِثِ",
+		latin : "Alloohumma Innii a'uudzubika minal khubutsi wal khoaaitsi",
+		indo : "“O God, I seek refuge in You from wickedness and evil things.”",
+		warna : 2
+	},
+
+];
